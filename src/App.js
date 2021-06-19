1a67325218cab0df54b2ae297a7fce75dcc4343d
katelyn chadwell <kchadwell0226@gmail.com>
2021-06-18 21:39:17 -0500

    imported Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 // import { BrowserRouter as Router, Route } from "react-router-dom";
-import Search from "./pages/search";
+import Header from "./components/Header";
 
 function App() {
-
-
+  return <Header />;
 }
 
 export default App;
