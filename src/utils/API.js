90185cc1a766e7e1cb6d13eef5e8bff7deb35ac3
katelyn chadwell <kchadwell0226@gmail.com>
2021-06-20 12:58:26 -0500

    added line to ignore eslint

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
 
+// eslint-disable-next-line
 export default {
     getEmployee: function () {
         return axios.get(BASEURL);
